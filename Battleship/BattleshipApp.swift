f7ed19e3e153dc02032bfc04d8183f710b3fec86
marilyne.leam <marilyne.leam@eemi.com>
2024-07-15 05:25:44 +0200

    Initial Commit

---
+++ Battleship/BattleshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattleshipApp.swift
+//  Battleship
+//
+//  Created by Marilyne LEAM on 15/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BattleshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
